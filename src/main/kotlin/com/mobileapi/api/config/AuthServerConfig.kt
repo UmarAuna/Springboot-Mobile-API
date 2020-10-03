ea6bac670fca786fff334b6a0aaeb768ba810f3a
Umar <umarmanofpeace@gmail.com>
2020-10-03 23:07:40 +0100

    feat: 🎸 Add MySql Database and User Model

--- src/main/kotlin/com/mobileapi/api/config/AuthServerConfig.kt
+++ src/main/kotlin/com/mobileapi/api/config/AuthServerConfig.kt
@@ -1,23 +1,36 @@
 package com.mobileapi.api.config
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer
+import org.springframework.security.oauth2.provider.token.TokenStore
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore
+import javax.sql.DataSource
 
 @Configuration
 @EnableAuthorizationServer
+@ComponentScan
 class AuthServerConfig : AuthorizationServerConfigurerAdapter() {
-    @Autowired
-    lateinit var passwordEncoder: BCryptPasswordEncoder
 
     @Autowired
-    lateinit var authenticationManager: AuthenticationManager
+    private lateinit var authenticationManager: AuthenticationManager
+
+    @Autowired
+    private lateinit var datasource: DataSource
+
+    @Autowired
+    private lateinit var userDetailsService: UserDetailsService
+
+    @Bean
+    fun tokenStore(): TokenStore = InMemoryTokenStore()
 
     @Throws(Exception::class)
     override fun configure(oauthServer: AuthorizationServerSecurityConfigurer) {
@@ -26,17 +39,13 @@ class AuthServerConfig : AuthorizationServerConfigurerAdapter() {
 
     @Throws(Exception::class)
     override fun configure(clients: ClientDetailsServiceConfigurer) {
-        clients.inMemory()
-                .withClient("client")
-                .secret(passwordEncoder.encode("secret"))
-                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
-                .scopes("user_info")
-                .autoApprove(true)
+        clients.jdbc(datasource)
     }
 
     @Throws(Exception::class)
     override fun configure(endpoints: AuthorizationServerEndpointsConfigurer) {
-        endpoints.authenticationManager(authenticationManager)
+        endpoints.tokenStore(tokenStore())
+                .userDetailsService(userDetailsService)
+                .authenticationManager(authenticationManager)
     }
-
 }
--- src/main/kotlin/com/mobileapi/api/config/DatabaseConfig.kt
+++ src/main/kotlin/com/mobileapi/api/config/DatabaseConfig.kt
@@ -1,4 +1,24 @@
 package com.mobileapi.api.config
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+@Configuration
 class DatabaseConfig {
+    @Value("\${spring.datasource.url}")
+    private lateinit var dbUrl: String
+
+
+    @Bean
+    fun dataSource(): DataSource? {
+        val config = HikariConfig()
+        config.jdbcUrl = dbUrl
+        return HikariDataSource(config)
+    }
+
+
 }
