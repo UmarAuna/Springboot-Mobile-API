3a04925cc3675a1c58c44d0fef7f96e66da42b96
Umar <umarmanofpeace@gmail.com>
2020-10-06 20:42:06 +0100

    feat: 🎸 created admin controller

--- src/main/kotlin/com/mobileapi/api/controller/admin/UserController.kt
+++ src/main/kotlin/com/mobileapi/api/controller/admin/UserController.kt
@@ -1,4 +1,21 @@
 package com.mobileapi.api.controller.admin
 
+import com.mobileapi.api.model.UserInfo
+import com.mobileapi.api.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+
+@RestController
+@RequestMapping("/admin")
 class UserController {
+    @Autowired
+    lateinit var userService: UserService
+
+    @RequestMapping(value = ["/users"], method = [(RequestMethod.GET)])
+    fun getAllUsersInfo(request: HttpServletRequest): ResponseEntity<List<UserInfo>> =
+            ResponseEntity.ok(userService.getAllUsers())
 }
